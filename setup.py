332f0262a4ed904fde35cd4e53dbe7c9588a9944
James Coder <jcoder@acadian-asset.com>
2020-01-07 10:13:06 -0500

    sync with upstream/stable

--- setup.py
+++ setup.py
@@ -173,11 +173,10 @@ cassandra = [
     'cassandra-driver>=3.13.0',
 ]
 celery = [
-    'celery~=4.2.1',
+    'celery~=4.3',
     'flower>=0.7.3, <1.0',
     'kombu==4.6.3;python_version<"3.0"',
     'tornado>=4.2.0, <6.0',  # Dep of flower. Pin to a version that works on Py3.5.2
-    'kombu==4.3.0'
 ]
 cgroups = [
     'cgroupspy>=0.1.4',
